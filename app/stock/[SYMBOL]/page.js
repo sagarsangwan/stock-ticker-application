f207f3adc9a8e7609003c4168349a957384727ac
sagar sangwan <iamsagarsangwan@gmail.com>
2025-08-14 20:14:38 +0530

    added metadata for both details page and main page

--- app/stock/[SYMBOL]/page.js
+++ app/stock/[SYMBOL]/page.js
@@ -1,23 +1,32 @@
 import StockDetailsCard from "@/components/stock-details-card";
 import StockPriceChart from "@/components/stock-price-chart";
+import { Button } from "@/components/ui/button";
 import { getStockPrices, searchStocks } from "@/lib/api";
+import { ArrowLeft, Heart } from "lucide-react";
 import React from "react";
+
+export async function generateMetadata({ params }) {
+  const { symbol } = await params;
+  const stockDetailsARR = await searchStocks(symbol, 1);
+  const stockDetails = stockDetailsARR[0];
+  return {
+    title: `${stockDetails.company} (${stockDetails.symbol}) | Stock Details`,
+    description: `Get live updates, price history, and details for ${stockDetails.company} (${stockDetails.symbol}).`,
+    keywords: `${stockDetails.company}, ${stockDetails.symbol}, stock price, ${stockDetails.symbol} share price, ${stockDetails.company} stock, NSE, BSE, Indian stock market`,
+  };
+}
 
 async function page({ params }) {
   const { symbol } = await params;
   const upperSymbol = symbol.toUpperCase();
-  const symbolDetails = await searchStocks(symbol, 1);
-  console.log(symbolDetails);
+  const stockDetails = await searchStocks(symbol, 1);
   const stockPrices = await getStockPrices(upperSymbol, 1);
-
   return (
     <div>
       <StockDetailsCard
-        symbolDetails={symbolDetails[0]}
-        stockPrices={stockPrices}
+        stockDetails={stockDetails[0]}
+        stockPrices={stockPrices.reverse()}
       />
-
-      {/* <StockPriceChart stockPrices={stockPrices.reverse()} /> */}
     </div>
   );
 }
