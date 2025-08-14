19d069cc947f07a45df72885b5b6072d88d135ed
sagar sangwan <iamsagarsangwan@gmail.com>
2025-08-15 01:46:27 +0530

    feat : added toploader for better user navigation

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader />
         {children}
       </body>
     </html>
