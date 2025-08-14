d9027792d6a6356ea8a40a532dafbe8ec2421af2
sagar sangwan <iamsagarsangwan@gmail.com>
2025-08-14 13:07:12 +0530

    search api api is now working

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,8 +1,28 @@
 import { NextResponse } from "next/server";
 
-export async function Get(request) {
+export async function GET(request) {
   const searchParams = request.nextUrl.searchParams;
-  console.log(searchParams);
-
-  return NextResponse({ message: "hiiii" });
+  const query = searchParams.get("query" || "");
+  const length = searchParams.get("length" || 10);
+  try {
+    const response = await fetch(
+      `https://portal.tradebrains.in/api/assignment/search?keyword=${query.toUpperCase()}&length=${length}`
+    );
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: "something went wrong" },
+        { status: 500 }
+      );
+    }
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      {
+        error: "something went wrong with external api",
+      },
+      { status: 500 }
+    );
+  }
 }
